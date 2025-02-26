652290fbaff17b1b72661d2f22d55ce124163c14
quang938848 <quangcsgo2005@gmail.com>
2025-02-26 13:33:46 +0700

    First commit

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace ett1_web_api.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Class { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ett1_web_api.Data;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Thêm services cho Controller, Swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 2. Lấy Connection String
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StudentContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+var app = builder.Build();
+
+// 3. Sử dụng Swagger (bật Swagger cho môi trường Development)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// 4. Map các Controller
+app.MapControllers();
+
+app.Run();
